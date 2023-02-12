4e67cb22e9c2488f5914f1c60e70bb92340beed0
seunghyun711 <seunghyun711@gmail.com>
2023-02-13 08:43:00 +0900

    AppConfig 리펙터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,10 +11,18 @@ import hello.core.order.OrderServiceImpl;
 // 애플리케이션 동작에 필요한 구현객체를 생성한다.
 public class AppConfig { // 의존관계는 여기에서 관리한다.
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
