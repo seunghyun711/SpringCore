3f7a20c98dcf3621269f8ea8dfd5ee405b693304
seunghyun711 <seunghyun711@gmail.com>
2023-02-13 08:25:54 +0900

    AppConfig로 애플리케이션 동작에 필요한 구현객체 생성

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+// 애플리케이션 동작에 필요한 구현객체를 생성한다.
+public class AppConfig { // 의존관계는 여기에서 관리한다.
+    public MemberService memberService(){
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
